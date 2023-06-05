d486de4479cdeb7f471b3f97465134e4391edb56
MD. Amimul Ahasun Anas <anasamimulahasun@gmail.com>
2023-06-05 16:16:36 +0600

    updated

--- demographics_data_analysis_using_pandas.py
+++ demographics_data_analysis_using_pandas.py
@@ -20,21 +20,21 @@ print(race_counts)
 
 #Average age of men
 average_age_men = dataset.loc[dataset['sex'] == 'Male', 'age'].mean()
-print("Average age of men:", average_age_men)
+print("Average age of men:", round(average_age_men, 2))
 
 #Percentage of people with a Bachelor's degree
 percentage_bachelors = (dataset['education'] == 'Bachelors').mean() * 100
-print("Percentage of people with a Bachelor's degree:", percentage_bachelors)
+print("Percentage of people with a Bachelor's degree:", round(percentage_bachelors, 2))
 
 #Percentage of people with advanced education making more than 50K
 advanced_education = dataset['education'].isin(['Bachelors', 'Masters', 'Doctorate'])
 percentage_advanced_education = (dataset.loc[advanced_education, 'salary'] == '>50K').mean() * 100
-print("Percentage of people with advanced education making more than 50K:", percentage_advanced_education)
+print("Percentage of people with advanced education making more than 50K:", round(percentage_advanced_education, 2))
 
 #Percentage of people without advanced education making more than 50K
-no_advanced_education = advanced_education
+no_advanced_education = ~advanced_education
 percentage_no_advanced_education = (dataset.loc[no_advanced_education, 'salary'] == '>50K').mean() * 100
-print("Percentage of people without advanced education making more than 50K:", percentage_no_advanced_education)
+print("Percentage of people without advanced education making more than 50K:", round(percentage_no_advanced_education, 2))
 
 #Minimum number of hours a person works per week
 min_hours_per_week = dataset['hours-per-week'].min()
@@ -42,13 +42,13 @@ print("Minimum number of hours a person works per week:", min_hours_per_week)
 
 #Percentage of people who work the minimum hours per week and have salary >50K
 min_hours_50k = (dataset.loc[dataset['hours-per-week'] == min_hours_per_week, 'salary'] == '>50K').mean() * 100
-print("Percentage of people who work the minimum hours per week and have salary >50K:", min_hours_50k)
+print("Percentage of people who work the minimum hours per week and have salary >50K:", round(min_hours_50k, 2))
 
 #Country with the highest percentage of people earning >50K and the percentage
 country_highest_50k = dataset.loc[dataset['salary'] == '>50K', 'native-country'].value_counts(normalize=True).idxmax()
 percentage_highest_50k = (dataset.loc[dataset['native-country'] == country_highest_50k, 'salary'] == '>50K').mean() * 100
 print("Country with the highest percentage of people earning >50K:", country_highest_50k)
-print("Percentage of people earning >50K in that country:", percentage_highest_50k)
+print("Percentage of people earning >50K in that country:", round(percentage_highest_50k, 2))
 
 #Most popular occupation for those earning >50K in India
 india_high_income_occupation = dataset.loc[(dataset['native-country'] == 'India') & (dataset['salary'] == '>50K'), 'occupation'].value_counts().idxmax()
